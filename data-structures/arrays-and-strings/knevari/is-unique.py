6fe2ad7972ce30fd6032f5e9e971224d248fa31f
Knevari <winresource000@gmail.com>
2022-08-25 16:10:01 -0300

    Add automation testing classes for python

--- data-structures/arrays-and-strings/knevari/is-unique.py
+++ data-structures/arrays-and-strings/knevari/is-unique.py
@@ -1,35 +1,32 @@
 # author: Knevari
 # 25/02/2022 13:43
 
+from helpers.python.Buzz import Buzz
+
+
 class Solution:
+    def __init__(self, testcases=[]):
+        self.testcases = testcases
+
     def is_unique(self, text: str):
         if text == "":
             return True
 
-        character_count = {}
+        text_characters = {}
 
         for char in text:
-            if char in character_count:
+            if char in text_characters:
                 return False
 
-            character_count[char] = True
+            text_characters[char] = True
 
         return True
 
 
 def main():
-    solution = Solution()
-
-    assert solution.is_unique("abcdefg") == True,\
-        "is_unique('abcdefg') should be True"
-
-    assert solution.is_unique("fffffff") == False,\
-        "is_unique('fffffff') should be False"
-
-    assert solution.is_unique("") == True,\
-        "is_unique('') should be True"
-
-    print("All tests passed!")
+    solution = Solution([("abcdefgg", True), ("fffffff", False), ("", True)])
+    buzz = Buzz(solution, solution.is_unique)
+    buzz.test()
 
 
 if __name__ == "__main__":
