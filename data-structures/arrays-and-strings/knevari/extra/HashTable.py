14747a3307e6459213fdcc2d41eb9a8999ea0e24
Knevari <winresource000@gmail.com>
2022-09-06 11:22:35 -0300

    Clean up HashTable

--- data-structures/arrays-and-strings/knevari/extra/HashTable.py
+++ data-structures/arrays-and-strings/knevari/extra/HashTable.py
@@ -1,5 +1,4 @@
 from hashlib import sha256
-from queue import PriorityQueue
 
 # Variables and methods starting with a single _
 # are intended to be "private"
@@ -10,12 +9,13 @@ class QueueItem:
         self.key = key
         self.value = value
         self.priority = priority
+        self._max_priority = 2 ** 6
 
     def increasePriority(self):
-        self.priority += 1
+        self.priority = min(self._max_priority, self.priority + 1)
 
     def decreasePriority(self):
-        self.priority -= 1
+        self.priority = max(0, self.priority + 1)
 
     def __repr__(self):
         return "{} - {}".format(str(self.value), str(self.priority))
@@ -23,23 +23,68 @@ class QueueItem:
 
 class PriorityQueue:
     def __init__(self):
-        self._heap = []
+        self._heap: list(QueueItem) = []
         self._starting_items_priority = 1
         self.size = 0
 
+    # Private Stuff
+    def _getLeftChildIndex(self, node_idx):
+        return 2 * node_idx + 1
+
+    def _getRightChildIndex(self, node_idx):
+        return 2 * node_idx + 2
+
+    def _getNodeKey(self, node_idx):
+        return self._heap[node_idx].key
+
+    def _getNodeValue(self, node_idx):
+        return self._heap[node_idx].value
+
+    def _getNodePriority(self, node_idx):
+        return self._heap[node_idx].priority
+
+    def _updateNodeKey(self, node_idx, key):
+        self._heap[node_idx].key = key
+
+    def _updateNodeValue(self, node_idx, value):
+        self._heap[node_idx].value = value
+
+    def _findNodeByKey(self, key):
+        idx = 0
+
+        for idx in range(self.size):
+            if self._compareNodesKeys(idx, key):
+                return idx
+
+        return -1
+
+    def _increaseNodePriority(self, node_idx):
+        self._heap[node_idx].increasePriority()
+
+    def _compareNodesKeys(self, node_idx, key):
+        self._heap[node_idx].key == key
+
+    def _swapNodes(self, first_node_idx, second_node_idx):
+        self._heap[first_node_idx], self._heap[second_node_idx] =\
+            self._heap[second_node_idx], self._heap[first_node_idx]
+
+    def _popLastElement(self):
+        self._heap.pop(self.size - 1)
+
     def _heapify(self, idx):
         largest = idx
-        left = 2 * idx + 1
-        right = 2 * idx + 2
 
-        if left < self.size and self._heap[idx].priority < self._heap[left].priority:
+        left = self._getLeftChildIndex(idx)
+        right = self._getRightChildIndex(idx)
+
+        if left < self.size and self._getNodePriority(idx) < self._getNodePriority(left):
             largest = left
 
-        if right < self.size and self._heap[largest].priority < self._heap[right].priority:
+        if right < self.size and self._getNodePriority(largest) < self._getNodePriority(right):
             largest = right
 
         if largest != idx:
-            self._heap[idx], self._heap[largest] = self._heap[largest], self._heap[idx]
+            self._swapNodes(largest, idx)
             self._heapify(largest)
 
     def _incrementSize(self):
@@ -48,47 +93,41 @@ class PriorityQueue:
     def _decrementSize(self):
         self.size -= 1
 
+    def _appendToHeap(self, key, value):
+        self._heap.append(QueueItem(key, value, self._starting_items_priority))
+
+    # Public Stuff
     def insert(self, key, value):
         if self.size == 0:
-            self._heap.append(
-                QueueItem(key, value, self._starting_items_priority))
+            self._appendToHeap(key, value)
         else:
-            print("key ", key, " value ", value)
-            self._heap.append(
-                QueueItem(key, value, self._starting_items_priority))
-            for i in range((self.size // 2) - 1, -1, -1):
-                self._heapify(i)
+            self._appendToHeap(key, value)
+            self._recalculateNodesPositions()
 
         self._incrementSize()
 
+    def _recalculateNodesPositions(self):
+        for idx in range((self.size // 2) - 1, -1, -1):
+            self._heapify(idx)
+
     def update(self, key, value):
         for idx in range(self.size):
-            if self._heap[idx].key == key:
-                self._heap[idx].value = value
-                self._heap[idx].increasePriority()
-
-                for i in range((self.size // 2) - 1, -1, -1):
-                    self._heapify(i)
+            if self._compareNodesKeys(idx, key):
+                self._updateNodeValue(idx, value)
+                self._increaseNodePriority(idx)
+                self._recalculateNodesPositions()
                 return True
 
             return False
 
     def remove(self, key):
-        idx = 0
+        idx = self._findNodeByKey(key)
 
-        for idx in range(self.size):
-            if key == self._heap[idx].key:
-                break
-
-        self._heap[idx], self._heap[self.size - 1] = \
-            self._heap[self.size - 1], self._heap[idx]
-
-        self._heap.pop(self.size - 1)
-
-        for i in range((self.size // 2) - 1, -1, -1):
-            self._heapify(i)
-
-        self._decrementSize()
+        if idx != -1:
+            self._swapNodes(idx, self.size - 1)
+            self._popLastElement()
+            self._decrementSize()
+            self._recalculateNodesPositions()
 
     def __iter__(self):
         return iter(self._heap)
@@ -98,10 +137,10 @@ class PriorityQueue:
 
 
 class HashTable:
-    def __init__(self, initial_size=1, growing_limit=100):
+    def __init__(self, initial_size=1, growing_limit=100, increasing_factor=2):
         self._size = 0
         self._capacity = initial_size
-        self._increasing_factor = 2
+        self._increasing_factor = increasing_factor
         self._growing_limit = growing_limit
         self._createBuckets()
 
